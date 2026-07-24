02368dc338d043482351dc8bf97584bb1e7202a5
MerlinManch <102369758+MerlinManch@users.noreply.github.com>
2026-07-24 09:44:12 +0200

    Art redesign: update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = 'last-lesson-v1';
+const CACHE = 'last-lesson-v2-art-redesign';
 const FILES = ['./','./index.html','./styles.css','./js/data.js','./js/logic.js','./js/save.js','./js/audio.js','./js/services.js','./js/game.js','./assets/app-icon-192.png','./assets/app-icon-512.png'];
 self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES))));
 self.addEventListener('activate', e => e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))));
